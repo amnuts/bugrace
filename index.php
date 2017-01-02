aac7e03dfd2b86cd11dda933102fd144c69b715a
Andrew Collington <andy@amnuts.com>
2017-01-02 21:18:04 +0000

    The page now generates the output automatically based on a geocache.loc file of all the way points a bug has been to and a list of each cache per person.  Treffynnon's navigator library was used (via composer) to make the coordinate calculations easier. The size of the cache trail font was made slightly smaller to allow for longer names no wrapping quite so much.

---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Treffynnon\Navigator as N;
+use Treffynnon\Navigator\Distance\Converter\MetreToKilometre as KM;
+use Treffynnon\Navigator\Distance\Converter\MetreToMile as M;
+
+$people = array_merge(
+    ['rosie' => [], 'luke' => [], 'lisa' => [], 'andy' => []],
+    @include __DIR__ . '/people.php'
+);
+
+$waypoints = [];
+if (file_exists(__DIR__.'/geocaching.loc')) {
+    $waypointXml = simplexml_load_file(__DIR__.'/geocaching.loc');
+    foreach ($waypointXml->waypoint as $waypoint) {
+        $waypoints[(string)$waypoint->name['id']] = [
+            'id'   => (string)$waypoint->name['id'],
+            'name' => preg_replace('/^(.*?) by .*\r?$/', '$1', trim((string)$waypoint->name)),
+            'lat'  => (string)$waypoint->coord['lat'],
+            'lon'  => (string)$waypoint->coord['lon']
+        ];
+    }
+}
+
+$maxTravelled = $maxVisited = 0;
+foreach ($people as $person => $caches) {
+    $data = [
+        'caches'    => [],
+        'visited'   => 0,
+        'travelled' => 0
+    ];
+    for ($i = 0; $i < count($caches); $i++) {
+        if (isset($waypoints[$caches[$i]])) {
+            $data['caches'][$i] = $waypoints[$caches[$i]];
+        } else {
+            $data['caches'][$i] = ['id' => $caches[$i]];
+        }
+        $data['caches'][$i]['travelled'] = 0;
+        if ($i && isset($data['caches'][$i]['lat']) && isset($data['caches'][$i-1]['lat'])) {
+            $data['caches'][$i]['travelled'] = N::getDistance(
+                $data['caches'][$i-1]['lat'], $data['caches'][$i-1]['lon'],
+                $data['caches'][$i]['lat'], $data['caches'][$i]['lon']
+            );
+            $data['travelled'] += $data['caches'][$i]['travelled'];
+        }
+    }
+    $data['visited'] = count($data['caches']);
+    $maxVisited = max($maxVisited, $data['visited']);
+    $maxTravelled = max($maxTravelled, $data['travelled']);
+    $people[$person] = $data;
+}
+
+$toMiles = new M();
+$toKilometres = new KM();
+
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Geocaching travel bug race</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-5122767-4', 'auto');
+    ga('send', 'pageview');
+</script>
+
+<header>
+    <div class="image-header">
+        <img src="img/hills.jpg"/>
+    </div>
+    <div class="row">
+        <div class="small-12 columns">
+            <h1>Geocaching Travel Bug Race</h1>
+            <h2>Help our bugs travel the world</h2>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="row">
+        <?php foreach ($people as $person => $data): ?>
+        <div class="large-3 medium-6 small-12 columns">
+            <div class="card">
+                <aside><span><?php echo ucfirst($person); ?></span></aside>
+                <img src="img/<?php echo $person; ?>.png" />
+                <div>
+                    <aside>
+                        <span><?php printf('%.02d', $toMiles->convert($data['travelled'])); ?></span>
+                        <span>mile<?php echo $toMiles->convert($data['travelled']) == 1 ? '' : 's'; ?></span>
+                        <?php if ($data['travelled'] && $data['travelled'] == $maxTravelled): ?>
+                        <span class="fa-stack winner-left">
+                            <i class="fa fa-certificate fa-spin fa-fw fa-stack-2x"></i>
+                            <i class="fa fa-trophy fa-fw fa-stack-1x"></i>
+                        </span>
+                        <?php endif; ?>
+                    </aside>
+                    <aside>
+                        <span><?php echo $data['visited']; ?></span>
+                        <span>cache<?php echo $data['visited'] == 1 ? '' : 's'; ?></span>
+                        <?php if ($data['visited'] > 1 && $data['visited'] == $maxVisited): ?>
+                        <span class="fa-stack winner-right">
+                            <i class="fa fa-certificate fa-spin fa-fw fa-stack-2x"></i>
+                            <i class="fa fa-trophy fa-fw fa-stack-1x"></i>
+                        </span>
+                        <?php endif; ?>
+                    </aside>
+                </div>
+            </div>
+            <?php if (!empty($data['caches'])): ?>
+            <ol class="trail hide-for-small-only">
+                <?php foreach ($data['caches'] as $i => $cache): ?>
+                    <? if ($i): ?>
+                    <aside><span><?php echo $cache['travelled'] ? sprintf('%.02dm', $toMiles->convert($cache['travelled'])) : '??'; ?></span></aside>
+                    <? endif; ?>
+                    <li>
+                        <a href="https://coord.info/<?php echo $cache['id']; ?>" target="_blank"><?php echo !empty($cache['name']) ? htmlentities($cache['name'], ENT_COMPAT, 'utf-8') : $cache['id']; ?></a>
+                        <? if (isset($cache['lat'])): ?>
+                        <p><b><?php echo $cache['lat']; ?> / <?php echo $cache['lon']; ?><!-- N 50° 50.040 E 000° 09.880 --></b><br/><?php echo $cache['id']; ?></p>
+                        <? endif; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ol>
+            <? endif; ?>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</main>
+
+<footer>
+    <div class="row">
+        <div class="small-12 medium-9 columns">
+            <p>Thanks for helping us make an adventure out of our travel bugs!</p>
+            <p>Please geocache safely and remember to always be respectful of the land.</p>
+        </div>
+        <div class="small-12 medium-3 columns">
+            <img src="img/cito.png" title="The 'Cache In Trash Out' Logo is a trademark of Groundspeak, Inc. Used with permission." />
+        </div>
+    </div>
+</footer>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
---
+++ people.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'rosie' => ['GCXCG6'],
+    'luke'  => ['GC54KRM'],
+    'lisa'  => ['GC6N2Q3'],
+    'andy'  => ['GC56JM7']
+];
