43617fd70a6e3d79997fc14e16b8217ebeb74c58
Andrew Collington <andy@amnuts.com>
2017-01-06 19:22:52 +0000

    New waypoint added for Rozalynd.

--- people.php
+++ people.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6']],
+    'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6', 'GC5Y27Q']],
     'luke'  => ['bug' => 'TB7T4R4', 'caches' => ['GC54KRM']],
     'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3']],
     'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7', 'GCXCG6']]
