9a6faf5120c290548aa6af83d5f631c9cfe55c4d
Andrew Collington <andy@amnuts.com>
2017-01-06 01:01:48 +0000

    Added first bug move!

--- people.php
+++ people.php
@@ -4,5 +4,5 @@ return [
     'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6']],
     'luke'  => ['bug' => 'TB7T4R4', 'caches' => ['GC54KRM']],
     'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3']],
-    'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7']]
+    'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7', 'GCXCG6']]
 ];
