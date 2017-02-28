6dd7bcd489ae3d258484280a363b5b0498475abb
Andrew Collington <andy@amnuts.com>
2017-02-28 22:26:35 +0000

    Added new cache info.

--- people.php
+++ people.php
@@ -3,6 +3,6 @@
 return [
     'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6', 'GC5Y27Q', 'GC5Q9RK', 'GC1Q1DK', 'GC3W0GA']],
     'luke'  => ['bug' => 'TB7T4R4', 'caches' => ['GC54KRM', 'GC6N2Q3', 'GC5RAKQ']],
-    'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3', 'GC6XJ1R']],
+    'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3', 'GC6XJ1R', 'GC70EF9']],
     'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7', 'GCXCG6']]
 ];
