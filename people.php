f206c287b5551e1230870289852ac6e30e86b3a9
Andrew Collington <andy@amnuts.com>
2017-03-08 00:34:56 +0000

    Rosie continues to kill it...

--- people.php
+++ people.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6', 'GC5Y27Q', 'GC5Q9RK', 'GC1Q1DK', 'GC3W0GA']],
+    'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6', 'GC5Y27Q', 'GC5Q9RK', 'GC1Q1DK', 'GC3W0GA', 'GC2FJDD']],
     'luke'  => ['bug' => 'TB7T4R4', 'caches' => ['GC54KRM', 'GC6N2Q3', 'GC5RAKQ']],
     'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3', 'GC6XJ1R', 'GC70EF9']],
     'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7', 'GCXCG6']]
