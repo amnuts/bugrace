ea4c576a6c1bd09bc18fae941fc8851d8f386218
Andrew Collington <andy@amnuts.com>
2017-03-21 22:57:58 +0000

    New caches added - Lisa is now killing it on number visited!

--- people.php
+++ people.php
@@ -1,8 +1,30 @@
 <?php
 
 return [
-    'rosie' => ['bug' => 'TB7T67K', 'caches' => ['GCXCG6', 'GC5Y27Q', 'GC5Q9RK', 'GC1Q1DK', 'GC3W0GA', 'GC2FJDD']],
-    'luke'  => ['bug' => 'TB7T4R4', 'caches' => ['GC54KRM', 'GC6N2Q3', 'GC5RAKQ']],
-    'lisa'  => ['bug' => 'TB7T3KE', 'caches' => ['GC6N2Q3', 'GC6XJ1R', 'GC70EF9']],
-    'andy'  => ['bug' => 'TB7T7F9', 'caches' => ['GC56JM7', 'GCXCG6']]
+    'rosie' => [
+        'bug' => 'TB7T67K',
+        'caches' => [
+            'GCXCG6', 'GC5Y27Q', 'GC5Q9RK', 'GC1Q1DK', 'GC3W0GA', 'GC2FJDD'
+        ]
+    ],
+    'luke'  => [
+        'bug' => 'TB7T4R4',
+        'caches' => [
+            'GC54KRM', 'GC6N2Q3', 'GC5RAKQ'
+        ]
+    ],
+    'lisa'  => [
+        'bug' => 'TB7T3KE',
+        'caches' => [
+            'GC6N2Q3', 'GC6XJ1R', 'GC70EF9',
+            'GC701DJ', 'GC71M4B', 'GC6VYGB', 'GC4DRJF', 'GC2R35D', 'GC1ZAHB', 'GC71VWE', 'GC6KNPH',
+            'GC4V2DE', 'GC6V2G8', 'GC7261Z', 'GC726YK', 'GC728A7', 'GC7286X', 'GC1DKZ6', 'GC3T7N1'
+        ]
+    ],
+    'andy'  => [
+        'bug' => 'TB7T7F9',
+        'caches' => [
+            'GC56JM7', 'GCXCG6'
+        ]
+    ]
 ];
