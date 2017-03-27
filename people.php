ed68d18abf60f58f1f6a161c7b38ff7b98a62f7b
Andrew Collington <a.p.collington@sussex.ac.uk>
2017-03-27 16:03:18 +0100

    More for Lisa!

--- people.php
+++ people.php
@@ -19,7 +19,7 @@ return [
             'GC6N2Q3', 'GC6XJ1R', 'GC70EF9',
             'GC701DJ', 'GC71M4B', 'GC6VYGB', 'GC4DRJF', 'GC2R35D', 'GC1ZAHB', 'GC71VWE', 'GC6KNPH',
             'GC4V2DE', 'GC6V2G8', 'GC7261Z', 'GC726YK', 'GC728A7', 'GC7286X', 'GC1DKZ6', 'GC3T7N1',
-            'GC701EP', 'GC6H2XT', 'GC71JV5'
+            'GC701EP', 'GC6H2XT', 'GC71JV5', 'GC72BKP', 'GC67JFG', 'GC63NZW'
         ]
     ],
     'andy'  => [
